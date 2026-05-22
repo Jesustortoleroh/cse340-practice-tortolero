80d2a32c3f3cccf822a75a7e756336783ab87934
Jesustortoleroh <tor19021@byui.edu>
2026-05-22 00:50:33 -0400

    Refactor catalog views and routes; add new list and detail templates

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -3,6 +3,20 @@ import { Router } from 'express';
 // Create a new router instance
 const router = Router();
 
+
+
+// Add catalog-specific styles to all catalog routes
+router.use('/catalog', (req, res, next) => {
+    res.addStyle('<link rel="stylesheet" href="/css/catalog.css">');
+    next();
+});
+
+// Add catalog-specific styles to all faculty routes
+router.use('/faculty', (req, res, next) => {
+    res.addStyle('<link rel="stylesheet" href="/css/faculty.css">');
+    next();
+});
+
 // Import statements for controllers and middleware
 import { addDemoHeaders } from '../middleware/demo/headers.js';
 import { catalogPage, courseDetailPage } from './catalog/catalog.js';
